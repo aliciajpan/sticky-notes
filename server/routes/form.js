86aa8c7f4132e5b236158123d576c08808d575e7
Brianna Dunfield <bdunfield@mta.ca>
2024-08-28 13:42:13 -0300

    posting for new comment

--- server/routes/form.js
+++ server/routes/form.js
@@ -1,5 +1,38 @@
-import express from 'express';
+import express, { json } from 'express';
+import { v4 } from 'uuid';
+import fs from 'node:fs';
 
 const formRouter = express.Router();
+formRouter.use(express.json());
+
+formRouter.post('/', (req, res) => {
+	const { name, signed, color, message } = req.body;
+	const id = v4();
+
+	const newMessage = {
+		id,
+		signed,
+		color,
+		message,
+	};
+
+    const newData = getPersonMessages(name, newMessage);
+    fs.writeFileSync("./data/notes.json", JSON.stringify(newData));
+    res.status(201).send(newData);
+});
+
+
+function getPersonMessages(name, newMessage){
+    const notesFile = fs.readFileSync("./data/notes.json");
+    const people = JSON.parse(notesFile);
+    const individualPerson = people.find((person)=> person.name === name);
+    console.log(individualPerson);
+    const personMessages = individualPerson.messages;
+    personMessages.push(newMessage);
+    individualPerson.messages = personMessages;
+    people.forEach((person)=>{person.name === name ? person = individualPerson : person});
+    return people;
+}
+
 
 export default formRouter;
