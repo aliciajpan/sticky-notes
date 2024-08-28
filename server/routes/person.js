b6af272f3ed051094f169c8cbdd21722457a4d86
Brianna Dunfield <bdunfield@mta.ca>
2024-08-28 12:57:29 -0300

    searches for specific person

--- server/routes/person.js
+++ server/routes/person.js
@@ -8,14 +8,23 @@ const personRouter = express.Router();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-personRouter.get("/:id", (req, res) => {
-    console.log(req.params);
-    const personFile = fs.readFileSync(path.join(__dirname, "../data/notes.json"));
-    const people = JSON.parse(personFile);
+personRouter.get('/:id', (req, res) => {
+	console.log(req.params);
+	const personFile = fs.readFileSync(
+		path.join(__dirname, '../data/notes.json')
+	);
+	const people = JSON.parse(personFile);
 
-    res.json(people);
-})
+	const individualPerson = people.find(
+		(person) => person.id === req.params.id
+	);
+
+	if (!individualPerson) {
+		res.status(404).send('Person not found');
+		return;
+	}
+
+	res.json(individualPerson);
+});
 
 export default personRouter;
-
-//sticky-notes\server\data\notes.json
