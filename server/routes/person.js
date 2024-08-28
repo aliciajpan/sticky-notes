6f9eaa8df1363ac9a275c736bf7549625b7cab8b
Brianna Dunfield <bdunfield@mta.ca>
2024-08-28 12:50:45 -0300

    able to get request to person/id and have data returned for that person

--- server/routes/person.js
+++ server/routes/person.js
@@ -1,5 +1,21 @@
 import express from 'express';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const personRouter = express.Router();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+personRouter.get("/:id", (req, res) => {
+    console.log(req.params);
+    const personFile = fs.readFileSync(path.join(__dirname, "../data/notes.json"));
+    const people = JSON.parse(personFile);
+
+    res.json(people);
+})
+
 export default personRouter;
+
+//sticky-notes\server\data\notes.json
